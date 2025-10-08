e22c4e5d158aa3f1e2decb5edfecafbbf5bb6821
TaylorCArmstrong <jtarmstrong2019@gmail.com>
2025-10-08 08:01:23 -0700

    added questions and loader

---
+++ script.js
@@ -0,0 +1,37 @@
+//Eg of how to write quiz data!
+
+const quizData = [
+    {
+        question: "Which of these is a primary color?",
+        options: ["orange", "purple", "green", "blue"],
+        answer: 3
+    },
+    {
+        question: "Who was the first president of the United States?",
+        options: ["Grover Clevland","Barak Obama","George Washington","Benjamin Franklin"],
+        answer: 2
+        
+    },
+    {
+        question: "Whats 9+10?",
+        options: ["19","21","20","25"],
+        answer: 0
+    },
+    {
+        question: "What is the 4th day of the week?",
+        options: ["Sunday","Wednesday","Friday","Tuesday"],
+        answer: 1
+    }
+];
+
+//How to load the question on the page
+
+currentQuestion.options.forEach((option, index) => {
+    const optionButton =
+    document.createElement('button');
+        optionButton.textContent = option;
+        optionButton.addEventListener('click', () => selectOption(index));
+        optionsContainer.appendChild(optionButton);
+
+});
+
